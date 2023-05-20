d7d04e83f089fed6ea1bdf62eccb11b20b56e960
johnnyb1694 <johnnyb1694@gmail.com>
2023-05-20 17:15:02 +0100

    Abstraction achieved

--- pi.R
+++ pi.R
@@ -12,7 +12,7 @@
 # 6. Unnecessary repetition: mainly the `ifelse()` calls (x)
 # 7. Lack of abstraction (e.g. functions); it is monolithic (x)
 # 8. Naive use of `:` sequence operator; better practice to use `seq_len()` or the like (x)
-# 9. No seed present: this is a simulation model after all!
+# 9. No seed present: this is a simulation model after all! (x)
 
 # Other practices which could help but deserve broader treatment:
 
@@ -22,24 +22,59 @@
 # 4. Testing
 
 # Setup
-n_trials <- 1000L
-min <- 0
-max <- 1
-set.seed(123)
+setup_parameters <- function(n_trials, min = 0, max = 1, seed = 123) {
+  
+  set.seed(seed)
+  
+  params <- c(seed = seed, n_trials = n_trials, min = min, max = max)
+  
+  return(params)
+}
 
 # Simulate points
-x <- runif(n = n_trials, min = min, max = max)
-y <- runif(n = n_trials, min = min, max = max)
+simulate_points <- function(params) {
+  
+  x <- runif(n = params[['n_trials']], min = params[['min']], max = params[['max']])
+  y <- runif(n = params[['n_trials']], min = params[['min']], max = params[['max']])
+  
+  points <- data.frame(x, y)
+  
+  return(points)
+}
 
 # Calculate distance to origin
-distance_to_origin <- sqrt(x^2 + y^2)
+compute_dist_to_origin <- function(points) {
+  
+  distance_to_origin <- sqrt(points[['x']]^2 + points[['y']]^2)
+  
+  return(distance_to_origin)
+}
 
 # Determine which points lie inside the circle
-inside <- (distance_to_origin <= 1)
+determine_classification <- function(distance_to_origin) {
+  
+  inside <- (distance_to_origin <= 1)
+  
+  return(inside)
+}
 
 # Calculate estimates of pi over time
-cumulative_inside <- cumsum(inside)
-pi_results <- (4 * cumulative_inside) / seq_len(n_trials)
+generate_pi_estimates <- function(inside) {
+  
+  n_trials <- length(inside)
+  
+  cumulative_inside <- cumsum(inside)
+  pi_results <- (4 * cumulative_inside) / seq_len(n_trials)
+  
+  return(pi_results)
+}
+
+# Pipeline-style execution
+estimates <- setup_parameters(n_trials = 100000L) |>
+  simulate_points() |>
+  compute_dist_to_origin() |>
+  determine_classification() |>
+  generate_pi_estimates()
 
 
 
