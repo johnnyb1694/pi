6060da9d716334f91e9b5db950c42385cd6e0f17
johnnyb1694 <johnnyb1694@gmail.com>
2023-05-20 16:44:34 +0100

    Parametrised the number of trials; now users only need to change this variable in one place, rather than several!

--- pi.R
+++ pi.R
@@ -4,19 +4,32 @@
 
 # The key problems with this script are as follows:
 
-# 1. The number of trials (or anything else) is not parametrised
+# 1. The number of trials (or anything else) is not parametrised as an integer 
 # 2. Inconsistent naming conventions
 # 3. Using loops in a naive way
 # 4. Growing objects in a loop in a naive way
 # 5. Failing to utilise inbuilt functionality (e.g. cumsum)
 # 6. Unnecessary repetition: mainly the `ifelse()` calls
-# 7. Lack of abstraction (e.g. functions)
+# 7. Lack of abstraction (e.g. functions); it is monolithic
+# 8. Naive use of `:` sequence operator; better practice to use `seq_len()` or the like
+# 9. No seed present: this is a simulation model after all!
 
-x <- runif(n = 1000, min = 0, max = 1)
-y <- runif(n = 1000, min = 0, max = 1)
+# Other practices which could help but deserve broader treatment:
+
+# 1. Virtual environments
+# 2. Configuration file
+# 3. Modularisation
+# 4. Testing
+
+n_trials <- 1000L
+min <- 0
+max <- 1
+
+x <- runif(n = n_trials, min = min, max = max)
+y <- runif(n = n_trials, min = min, max = max)
 
 distances <- c()
-for (i in 1:1000) {
+for (i in 1:n_trials) {
   distances <- c(distances, sqrt(x[i] ^ 2 + y[i] ^ 2))
 }
 
@@ -25,7 +38,7 @@ flagInsideOrOutside <- ifelse(distances <= 1, 'Inside', 'Outside')
 inside <- ifelse(flag == 'Inside', 1, 0)
 
 cumulative_inside <- c()
-for (i in 1:1000) {
+for (i in 1:n_trials) {
   if (i == 1) {
     cumulative_inside[1] <- inside[1]
   } else {
@@ -34,7 +47,7 @@ for (i in 1:1000) {
 }
 
 piResults <- c()
-for (i in 1:1000) {
+for (i in 1:n_trials) {
   numberOfPointsInsideTheSquare <- i
   piResults <- c(piResults, 4 * cumulative_inside[i] / numberOfPointsInsideTheSquare)
 }
