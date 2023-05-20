4b43e65bfa2e7d87dedaeaa46d1b9641f946137e
johnnyb1694 <johnnyb1694@gmail.com>
2023-05-20 17:01:28 +0100

    Got rid of terrible loops, growing objects; added use of inbuilt cumsum

--- pi.R
+++ pi.R
@@ -6,9 +6,9 @@
 
 # 1. The number of trials (or anything else) is not parametrised as an integer (x)
 # 2. Inconsistent naming conventions (x)
-# 3. Using loops in a naive way
-# 4. Growing objects in a loop in a naive way
-# 5. Failing to utilise inbuilt functionality (e.g. cumsum)
+# 3. Using loops in a naive way (x)
+# 4. Growing objects in a loop in a naive way (x)
+# 5. Failing to utilise inbuilt functionality (e.g. cumsum) (x)
 # 6. Unnecessary repetition: mainly the `ifelse()` calls
 # 7. Lack of abstraction (e.g. functions); it is monolithic
 # 8. Naive use of `:` sequence operator; better practice to use `seq_len()` or the like
@@ -28,28 +28,15 @@ max <- 1
 x <- runif(n = n_trials, min = min, max = max)
 y <- runif(n = n_trials, min = min, max = max)
 
-distance_to_origin <- c()
-for (i in 1:n_trials) {
-  distance_to_origin <- c(distance_to_origin, sqrt(x[i] ^ 2 + y[i] ^ 2))
-}
+distance_to_origin <- sqrt(x^2 + y^2)
 
 flag_inside_or_outside <- ifelse(distance_to_origin <= 1, 'Inside', 'Outside')
 
 inside <- ifelse(flag_inside_or_outside == 'Inside', 1, 0)
 
-cumulative_inside <- c()
-for (i in 1:n_trials) {
-  if (i == 1) {
-    cumulative_inside[1] <- inside[1]
-  } else {
-    cumulative_inside[i] <- cumulative_inside[i - 1] + inside[i]
-  }
-}
+cumulative_inside <- cumsum(inside)
 
-pi_results <- c()
-for (i in 1:n_trials) {
-  pi_results <- c(pi_results, 4 * cumulative_inside[i] / i)
-}
+pi_results <- (4 * cumulative_inside) / seq_len(n_trials)
 
 
 
